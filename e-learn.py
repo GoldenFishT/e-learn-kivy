87a2598f6cd07335bc3a781830ee6482a769764d
tong <446835798@qq.com>
2024-06-26 22:52:42 +0900

    add lesson info page

--- e-learn.py
+++ e-learn.py
@@ -2,22 +2,56 @@ import kivy
 kivy.require('2.3.0')
 
 from kivy.app import App
-from kivy.uix.button import Button, Label
+from kivy.uix.button import Button
 from kivy.uix.recycleview import RecycleView
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.recycleview.views import RecycleDataViewBehavior 
-from kivy.properties import OptionProperty, StringProperty
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.screenmanager import ScreenManager, Screen 
+from kivy.properties import OptionProperty, StringProperty, NumericProperty
 from kivy.uix.popup import Popup
 
 from models import lessonInfos
 
+class LessonListScreen(Screen):
+    pass
+class LessonInfoScreen(Screen):
+    lesson_id = NumericProperty(0)
+    lesson_state = OptionProperty("Unenrolled", options=["Unenrolled", "Learning", "Finished"])
+    name = StringProperty('LessonName')
+    lesson_description = StringProperty('lesson_description')
+    instructor = StringProperty('instructor')
+    action = StringProperty('Enroll')
+
+    def __init__(self, lesson_id=0, **kw):
+        super(LessonInfoScreen, self).__init__(**kw)
+        self.lesson_id = lesson_id
+        self.lesson = lessonInfos[lesson_id]
+        self.name = self.lesson.name
+        self.lesson_state = self.lesson.lesson_state
+        self.instructor = self.lesson.instructor
+        self.lesson_description = self.lesson.description
+        self.action = self.get_action(self.lesson_state)
+
+    def get_action(self, state):
+        match state:
+            case 'Unenrolled':
+                return 'Enroll'
+            case 'Learning':
+                return 'Resume'
+            case 'Finished':
+                return 'Review'
+    
+
+class LessonContentScreen(Screen):
+    pass
+
 class LessonInfoRV(RecycleView):
     def __init__(self, **kwargs):
         super(LessonInfoRV, self).__init__(**kwargs)
-        self.data = [{'lesson_state': str(x.lesson_state), 'name': str(x.name)} for x in lessonInfos]
+        self.data = [{'lesson_state': str(x.lesson_state), 'name': str(x.name), 'id': x.id} for x in lessonInfos]
 
 class LessonInfoButton(RecycleDataViewBehavior, BoxLayout, Button):
+    lesson_id = NumericProperty(0)
     lesson_state = OptionProperty("Unenrolled", options=["Unenrolled", "Learning", "Finished"])
     name = StringProperty('LessonName')
     index = 0
@@ -25,20 +59,33 @@ class LessonInfoButton(RecycleDataViewBehavior, BoxLayout, Button):
     def refresh_view_attrs(self, rv, index, data):
         self.index = index
         self.lesson_state = data['lesson_state']
+        self.lesson_id = data['id']
         self.name = data['name']
         print(self.lesson_state, self.name)
         super(LessonInfoButton, self).refresh_view_attrs(rv, index, data)
 
-    def show_info(self):
-        the_content = Label(text="coming soon!")
-        popup = Popup(title='Lesson Information', content=the_content,
-                      size_hint=(.6, .3))
-        popup.open()
-
+    def show_info(self, id):
+        App.get_running_app().to_lesson_info_screen(id)
 
 class ElearnApp(App):
     def build(self):
-        return LessonInfoRV()
+        sm = ScreenManager()
+        sm.add_widget(LessonListScreen(name="Lesson List"))
+        sm.add_widget(LessonInfoScreen(name="Lesson Information"))
+        sm.add_widget(LessonContentScreen(name="Lesson Content"))
+        self.sm = sm
+
+        return sm
+    
+    def to_lesson_info_screen(self, id):
+        screen_name = f'lesson info {id}'
+        # print('screen_name: ', screen_name)
+        if not self.sm.has_screen('screen_name'):
+            screen = LessonInfoScreen(lesson_id=id, name=screen_name)
+            self.sm.add_widget(screen)
+            self.sm.switch_to(screen)
+        else:
+            self.sm.current = screen_name
 
 if __name__ == '__main__':
     ElearnApp().run()
--- models.py
+++ models.py
@@ -5,12 +5,19 @@ class Lesson:
 STATES = ["Unenrolled", "Learning", "Finished"]
 
 class LessonInfo:
-    
+    _id = 0
 
-    def __init__(self, name, lesson_state, tags=None) -> None:
+    def __init__(self, name, lesson_state, instructor=None, tags=None, description=None) -> None:
+        self.id = LessonInfo._id
+        LessonInfo._id += 1
         self.name = name
         self.lesson_state = lesson_state
-        self.tags = tags
+        self.instructor = instructor if instructor else 'instructor name'
+        self.tags = tags if tags else []
+        self.description = description if description else ""
+
+    def set_description(self, description):
+        self.description = description
 
     def __repr__(self):
         return str(self.name)
@@ -19,9 +26,11 @@ class LessonInfo:
 
 class LessonContent:
     pass
+    
+
 
 TAGS = ['math', 'computer', 'science', 'geometry', 'analysis', 'algorithm', 'introduction']
 
 lessonInfos = [
-    LessonInfo(name=f'lesson name{i}', lesson_state=random.choice(STATES), tags=random.sample(TAGS, random.randint(1, len(TAGS)))) for i in range(20)
+    LessonInfo(name=f'lesson name{i}', lesson_state=random.choice(STATES), tags=random.sample(TAGS, random.randint(0, len(TAGS)))) for i in range(20)
 ]
